3925fd2f286ac878278ea100c3e70c77a507d6ed
Artur <hello@arturneufeld.com>
2023-08-24 18:19:08 +0200

    new share image

--- assets/data/meta.js
+++ assets/data/meta.js
@@ -4,7 +4,7 @@ const meta = {
   defaultDescription:
     "Kimera is a type foundry, offering retail and bespoke typefaces for international clients. Kimera is a design studio, providing art direction in the fields of culture, art and lifestyle. Kimera is a store for various products. Kimera is Unity in Multiplicity.",
   defaultShareImage:
-    "https://a.storyblok.com/f/167503/1366x768/d3d71db19e/ogshareimage.jpg",
+    "https://a.storyblok.com/f/167503/1366x768/6f7db3ddee/kimera_website_share_image_015.jpg",
 };
 
 export default meta;
